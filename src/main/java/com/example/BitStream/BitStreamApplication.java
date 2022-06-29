52ed560398094f35e302138f25c3bd6fceee9456
DheerajR8 <dheerajraikwar009@gmail.com>
2022-06-29 20:32:22 +0530

    adding conf.

--- src/main/java/com/example/BitStream/BitStreamApplication.java
+++ src/main/java/com/example/BitStream/BitStreamApplication.java
@@ -3,11 +3,14 @@ package com.example.BitStream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import com.example.BitStream.FileHandling.StorageService;
+import com.example.BitStream.FileHandling.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BitStreamApplication {
 
 	public static void main(String[] args) {

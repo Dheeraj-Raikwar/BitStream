9351905ea20556010c0d53f81068fbe6f49042c9
Dheeeraj-Raikwar <dheerajraikwar009@gmail.com>
2023-11-07 23:16:34 +0530

    logging added in main file

--- src/main/java/com/example/BitStream/BitStreamApplication.java
+++ src/main/java/com/example/BitStream/BitStreamApplication.java
@@ -5,12 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class BitStreamApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BitStreamApplication.class);
 
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(BitStreamApplication.class, args);
+		}
+		catch(Exception e){
+//			logger.info("This is an informational message.");
+	        logger.error("This is an exception message.",e);
+		}
+		
 	}
 
 }

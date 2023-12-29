6ad9f4a94332071289a3deebc02f73c9215053ba
dheeraj-raikwar <dheerajraikwar009@gmail.com>
2023-12-29 18:37:14 +0530

    app.js layout updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,27 @@ import { useNavigate } from 'react-router-dom'; // Import useNavigate
 // import AuthVerify from "./common/auth-verify";
 import EventBus from "./common/EventBus";
 import { useState, useEffect } from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Layout, Flex, Menu, theme, Button } from 'antd';
+import { useLocation } from 'react-router-dom';
 
-const { Header, Content, Footer } = Layout;
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  GlobalOutlined,
+  FireOutlined,
+  ToTopOutlined,
+  FolderOutlined,
+  StarOutlined,
+  UserOutlined
+} from '@ant-design/icons';
+
+const { Header, Sider, Content, Footer } = Layout;
+
 const App = () => {
+  const authService = AuthService();
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   const [showAdminBoard, setShowAdminBoard] = useState(false);
   const [currentUser, setCurrentUser] = useState(undefined);
   // Retrieve the last selected menu item from localStorage
@@ -26,34 +43,13 @@ const App = () => {
   // Initialize state with the stored value or the default value
   const [currentSelectedItem, setCurrentSelectedItem] = useState(storedSelectedItem || '2');
 
-  const authService = AuthService();
-  useEffect(() => {
+  const [collapsed, setCollapsed] = useState(false);
 
-    try {
-      const user = authService.getCurrentUser();
-      if (user) {
-        setCurrentUser(user);
-        setShowAdminBoard(user.roles.includes('ROLE_ADMIN'));
-      }
-    } catch (error) {
-      console.error('Login failed', error);
-    }
+  // check url path
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-    const handleLogout = () => {
-      logOut();
-    };
-
-    EventBus.on('logout', handleLogout);
-
-    localStorage.setItem('selectedMenuItem', currentSelectedItem);
-
-    return () => {
-      EventBus.remove('logout', handleLogout);
-    };
-
-  }, [currentSelectedItem]); // Empty dependency array to mimic componentDidMount
-
-  var menuItems = [
+  var headerItems = [
     // {
     //   key: '1',
     //   label: 'Bitstream',
@@ -105,28 +101,121 @@ const App = () => {
 
   ].filter(Boolean);
 
-  const navigate = useNavigate();
+  const [sidebarItems, setSidebarItems] = useState([]);
+
+
+  useEffect(() => {
+
+    try {
+      const user = authService.getCurrentUser();
+      if (user) {
+        setCurrentUser(user);
+        setShowAdminBoard(user.roles.includes('ROLE_ADMIN'));
+      }
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+
+    const handleLogout = () => {
+      logOut();
+    };
+
+    EventBus.on('logout', handleLogout);
+
+    localStorage.setItem('selectedMenuItem', currentSelectedItem);
+    console.log(currentSelectedItem);
+
+    if (currentSelectedItem === '2') {
+      setSidebarItems([{
+        key: '1',
+        icon: <GlobalOutlined />,
+        label: 'All Video',
+        path: '/all'
+      },
+
+      {
+        key: '2',
+        icon: <FireOutlined />,
+        label: 'Popular',
+        path: '/nowplaying'
+      }
+      ])
+    }
+    else if (currentSelectedItem === '4') {
+      setSidebarItems([{
+        key: '1',
+        icon: <ToTopOutlined />,
+        label: 'Upload',
+        path: '/user'
+      },
+
+      {
+        key: '2',
+        icon: <FolderOutlined />,
+        label: 'My Videos',
+        path: '/myvideos'
+      },
+
+      {
+        key: '3',
+        icon: <StarOutlined />,
+        label: 'Favourite',
+        path: '/favourite'
+      }
+
+      ])
+
+    }
+    else if (currentSelectedItem === '5') {
+      setSidebarItems([{
+        key: '1',
+        icon: <UserOutlined />,
+        label: 'Profile',
+        path: '/profile'
+      }
+      ])
+    }
+    else {
+      setSidebarItems([{
+        key: '1',
+        icon: <GlobalOutlined />,
+        label: 'All Video',
+        path: '/all'
+      }])
+    }
+
+    return () => {
+      EventBus.remove('logout', handleLogout);
+    };
+
+  }, [currentSelectedItem]); // Empty dependency array to mimic componentDidMount
+
+
   const logOut = () => {
     authService.logout();
     setShowAdminBoard(false);
     setCurrentUser(undefined);
   };
 
+  console.log(sidebarItems)
+
   return (
     <div>
       <div>
-        <Layout className="layout">
+        <Layout>
+          {/* Ant design header */}
           <Header style={{ display: "flex", justifyContent: 'space-between', alignItems: "center" }}>
-            <div className="demo-logo"></div>
-            {/* <Link to="/">
-              <div className="demo-logo" style={{ width: '50px', height: '50px', background: 'url(https://img.freepik.com/free-vector/colorful-bird-illustration-gradient_343694-1741.jpg?w=740&t=st=1701947726~exp=1701948326~hmac=60636e74737582b51f7585925cd63988c5e8f6117a169a5060c182e9c8d9615f.png) no-repeat', backgroundSize: 'cover' }} />
-            </Link> */}
+            <div className="demo-logo">
+              <Link to="/">
+                <div className="demo-logo" style={{ width: '30px', height: '30px', background: `url(${process.env.PUBLIC_URL}/logo192.png) no-repeat`, backgroundSize: 'cover' }} />
+              </Link>
+            </div>
             <Menu
               theme="dark"
               mode="horizontal"
               defaultSelectedKeys={[currentSelectedItem]}
               onClick={({ key }) => setCurrentSelectedItem(key)}
-              items={menuItems.map(item => ({
+              items={headerItems.map(item => ({
                 key: item.key,
                 label: item.key === '6' ? (
                   <a key={item.key} onClick={logOut}>
@@ -141,18 +230,73 @@ const App = () => {
               className="custom-menu"
             />
           </Header>
-          <div>
-            <Routes>
-              <Route exact path="/*" element={<Home />} />
-              <Route exact path="/home/*" element={<Home />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/register" element={<Register />} />
-              <Route exact path="/profile" element={<Profile />} />
-              <Route path="/user/*" element={<BoardUser />} />
-              <Route path="/admin" element={<BoardAdmin />} />
-              <Route path="/error" element={<Error />} />
-            </Routes>
-          </div>
+
+          {/* Ant design side bar*/}
+
+          <Layout style={{ minHeight: '100vh' }}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+              <div className="demo-logo-vertical" />
+              <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                items={sidebarItems.map(item => ({
+                  key: item.key,
+                  icon: item.icon,
+                  label:
+                    <Link key={item.key} to={item.path}>
+                      {item.label}
+                    </Link>
+                }))}
+              />
+            </Sider>
+
+            <Layout>
+              <Header
+                style={{
+                  padding: 0,
+                  background: colorBgContainer,
+                }}
+              >
+                <Button
+                  type="text"
+                  icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                  onClick={() => setCollapsed(!collapsed)}
+                  style={{
+                    fontSize: '16px',
+                    width: 64,
+                    height: 64,
+                  }}
+                />
+              </Header>
+
+              <Content
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                  minHeight: 280,
+                  background: colorBgContainer,
+                  borderRadius: borderRadiusLG,
+                }}
+              >
+
+                <div>
+                  <Routes>
+                    <Route exact path="/*" element={<Home />} />
+                    <Route exact path="/home/*" element={<Home />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/register" element={<Register />} />
+                    <Route exact path="/profile" element={<Profile />} />
+                    <Route path="/user/*" element={<BoardUser />} />
+                    <Route path="/admin" element={<BoardAdmin />} />
+                    <Route path="/error" element={<Error />} />
+                  </Routes>
+                </div>
+
+              </Content>
+            </Layout>
+
+          </Layout>
           <Footer style={{ textAlign: "center" }}>
             Ant Design ©2024 Created by &
           </Footer>
